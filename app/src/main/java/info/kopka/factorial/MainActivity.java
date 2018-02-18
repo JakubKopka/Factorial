63a92526d8aa805d078d9aee169836cfcd5083cf
Jakub Kopka <kuba@surfportal.pl>
2018-02-18 17:58:04 +0100

    Initial commit

---
+++ app/src/main/java/info/kopka/factorial/MainActivity.java
@@ -0,0 +1,52 @@
+package info.kopka.factorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BigInteger number = new BigInteger(editText.getText().toString());
+                BigInteger factorial = factorial(number);
+                String result = String.format("Factorial of %d is equal to %d", number, factorial);
+                textView.setText(result);
+            }
+        });
+    }
+
+    /**
+     * Recursive method for the calculation of factorial based on BigInteger.
+     * @param n
+     * @return Factorial of n.
+     */
+    private BigInteger factorial(BigInteger n){
+        if (n.compareTo(BigInteger.ONE) == -1){
+            return BigInteger.ONE;
+        } else{
+            return n.multiply(factorial(n.subtract(BigInteger.ONE)));
+        }
+    }
+}
